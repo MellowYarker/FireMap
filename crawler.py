019877862ab230bd4f8417798e08c3a075a4501f
MellowYarker <milanmiladinovic@outlook.com>
2017-07-07 14:09:17 -0400

    Changed default amount of tweets to collect

--- crawler.py
+++ crawler.py
@@ -10,7 +10,7 @@ consumer = oauth.Consumer(key=consumer_key, secret=consumer_secret)
 access_token = oauth.Token(key=access_token, secret=access_token_secret)
 client = oauth.Client(consumer, access_token)
 
-timeline_endpoint = 'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=tofire&count=30&exclude_replies=true&include_rts=false'
+timeline_endpoint = 'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=tofire&count=10&exclude_replies=true&include_rts=false'
 
 response, data = client.request(timeline_endpoint)
 
@@ -20,4 +20,5 @@ with open("extracted.txt", 'w') as f:
     for tweet in tweets:
         f.write(str(tweet['text']) + '\n')
     # print(tweet['text'], tweet['created_at'])
+
 
