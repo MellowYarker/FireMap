062589877bdbfb3d7d047513b7f1a5b54be281b6
MellowYarker <milanmiladinovic@outlook.com>
2017-07-10 23:18:23 -0400

    Corrected removal of tweets that have been updated

--- currator.py
+++ currator.py
@@ -47,18 +47,21 @@ def check_type_of_tweet(tweets):
 
     Determine the type of tweet, then call the appropriate function
 
-    >>> tweets = ['Alarm Highrise Residential - Eglinton Avenue b/w Centre St / Markham Road, Scarborough (6 Trucks)', 'UD: Vehicle (Personal Injury Highway) -  North York (3 Trucks)', 'UD: Vehicle (Personal Injury Highway) -  North York (3 Trucks)']
-    >>> print(check_type_of_tweet(tweets))
+    >>> test = ['UD: Medical (Unconscious) - Drovers Lane @ Verner Lane, Toronto (3 Trucks)', 'Medical (Unconscious) - Drovers Lane @ Verner Lane, Toronto (2 Trucks)']
+    >>> print(check_type_of_tweet(test))
+    >>> tweet = ['Check Call - Yonge St b/w Asquith Avenue / Yorkville Avenue, Toronto (3 Trucks)']
+    >>> print(check_type_of_tweet(tweet))
+
     """
 
+    event_list = []
     updated_count = 0
-    tweets = tweets[::-1]
     for tweet in tweets:
 
         # This flag represents if a tweet has already been updated
         updated = False
 
-        # Determine if tweet begins with UD
+        # Check if it is a valid event
         if '-' in tweet and tweet[0] != '[':
             # Check updated tweet
             if tweet[0:4] == 'UD: ':
@@ -77,11 +80,22 @@ def check_type_of_tweet(tweets):
                         event_list.append(formatted)
 
             elif '-' and ',' in tweet:
-                event_list.append(regular(tweet))
+                formatted = regular(tweet)
+                if len(event_list) == 0:
+                    event_list.append(formatted)
+                else:
+                    for event in event_list:
+                        if formatted[1] in event and (formatted[3] in event or
+                                                      formatted[3] == 'Other'):
+                            updated = True
+                            updated_count += 1
+                    if updated is False:
+                        event_list.append(formatted)
+
     if updated_count > 0:
         with open("updates.txt", 'w') as f:
             f.write(str(updated_count))
-    return event_list[::-1]
+    return event_list
 
 
 def regular(tweet):
@@ -94,8 +108,9 @@ def regular(tweet):
     >>> regular('Alarm Highrise Residential - Eglinton Avenue b/w Centre St / Markham Road, Scarborough (6 Trucks)')
     ['Alarm Highrise Residential', 'Eglinton Avenue and Centre St', 6, 'Alarm']
     """
-
+    # print(tweet)
     lst = []
+    strip_newline(tweet)
     level = None
     # Check if there is an alarm level
     if 'Alm]' in tweet:
@@ -119,7 +134,6 @@ def regular(tweet):
 
     # Remove irrelevant part of location
     cut(lst)
-    # print(lst)
     first = lst[0].split()[0]
     if 'Alarm' in lst[0]:
         lst.append('Alarm')
@@ -132,8 +146,8 @@ def regular(tweet):
     else:
         lst.append('Other')
     lst.append(level)
+    # print(lst)
     return lst
-# print(regular('Alarm Highrise Residential - Lake Shore Boulevard b/w Third St / Fourth St, Etobicoke (10 Trucks)'))
 
 
 def ud_tweet(tweet):
@@ -150,7 +164,7 @@ def ud_tweet(tweet):
     """
 
     lst = []
-
+    strip_newline(tweet)
     # Remove first 4 characters
     tweet = tweet[4:]
     level = None
@@ -194,7 +208,11 @@ def ud_tweet(tweet):
             lst.append('Other')
         lst.append(level)
         return lst
-# print(ud_tweet('UD: Alarm Highrise Residential - Lake Shore Boulevard b/w Third St / Fourth St, Etobicoke (10 Trucks)'))
+
+
+def strip_newline(tweet):
+    """Remove redundant newlines in a tweet"""
+    tweet = tweet[:-1]
 
 
 def unknown(tweet):
@@ -207,8 +225,10 @@ def reformat(tweet):
     fails due to difficult location descriptions.
 
     >>> a = 'Martin Grove Road and Martin Grove South Albion West Ramp'
-    >>> b = 'South Toronto'
+    >>> b = 'Calderstone Crescent and Dear Gt'
     >>> reformat(a)
+    >>> reformat(b)
+
 
     """
     # Remove n, s, e, w
@@ -217,15 +237,25 @@ def reformat(tweet):
         if i in tweet:
             tweet = tweet.replace(i, '')
 
+    # Turn Gt into Gate
+    if 'Gt' in tweet:
+        tweet = tweet.replace('Gt', 'Gate')
+
     # Remove reoccuring street names
     tweet = tweet.split()
     temporary = []
     for i in tweet:
         if i not in temporary:
             temporary.append(i)
+
     # Put it all back together in a new string
     final_string = ''
     for i in temporary:
         final_string += str(i) + ' '
 
     return final_string
+
+if __name__ == '__main__':
+    ultimate = ['Medical (Chest Pains) - Woolner Avenue b/w Rockcliffe Boulevard / Jane St, York (2 Trucks)\n', 'Alarm Residential - Coxwell Avenue b/w Dundas St East / Robbins Avenue, Toronto (6 Trucks)\n', 'UD: Vehicle (Personal Injury) - Weston Road @ St Phillips Road, York (2 Trucks)\n', 'Public Hazard - Weston Road @ St Phillips Road, York (2 Trucks)\n', 'UD: Medical (Unconscious) - Drovers Lane @ Verner Lane, Toronto (3 Trucks)\n', 'Medical (Unconscious) - Drovers Lane @ Verner Lane, Toronto (2 Trucks)\n', 'Medical (Vsa) - Phillip Avenue b/w Mcintosh St / Craiglee Dr, Scarborough (2 Trucks)\n', 'Fire (Grass/rubbish) - (Tedford Dr @ Clearfield Gt), Scarborough (2 Trucks)\n', 'Medical (Unconscious) - Chapman Avenue b/w Avis Crescent / Munford Crescent, East York (2 Trucks)\n', 'Medical (Vsa) - Calderstone Crescent b/w Dear Gt / Kirkdene Dr, Scarborough (2 Trucks)\n', 'Medical (Trouble Breathing) - Lawrence Avenue b/w Townley Avenue / Pharmacy Avenue, Scarborough (2 Trucks)\n', 'Medical (Unconscious) - Dundas St b/w Roncesvalles Avenue / Bloor St West, Toronto (2 Trucks)\n', 'Medical (Unconscious) - Victoria Park Avenue b/w Esquire Road / Sheppard Avenue East, Scarborough (2 Trucks)\n', 'Medical (Other) - Morningside Avenue b/w Military Trail / Tams Road, Scarborough (2 Trucks)\n', 'Check Call - Yonge St b/w Asquith Avenue / Yorkville Avenue, Toronto (3 Trucks)\n', 'Medical (Allergy) - Thorncliffe Park Dr b/w West Don River Trail / West Don River Trail, East York (2 Trucks)\n', 'Medical (Trouble Breathing) - Wiley Avenue b/w Milverton Boulevard / Sammon Avenue, East York (2 Trucks)\n', 'Tems Transferred (Read Remarks) - Rathburn Road b/w Melbert Road / Elmcrest Creek Trail, Etobicoke (2 Trucks)\n', 'UD: Vehicle (Personal Injury Highway) -  North York (5 Trucks)\n', 'Fire (Grass/rubbish) - Queen St b/w Jameson Avenue / Macdonell Avenue, Toronto (2 Trucks)\n']
+    [print(i[1]) for i in check_type_of_tweet(ultimate)]
+    # print(check_type_of_tweet(ultimate))
