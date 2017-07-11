ef3fd577ab5ecc14c65a042c4b4de1de9ec05435
MellowYarker <milanmiladinovic@outlook.com>
2017-07-10 23:17:35 -0400

    Changed amount of tweets extracted

--- crawler.py
+++ crawler.py
@@ -1,16 +1,16 @@
 import json
 import oauth2 as oauth
-consumer_key = 'YOURKEY'
-consumer_secret = 'YOURSECRET'
+consumer_key = 'PcbOwOjAbWhptLNby9ZAOwYyX'
+consumer_secret = 'VNUAhklgxH5RDZ3qan4mjRoXgcTKhJiDGb4ls6vQ6RB7cYdN2m'
 
-access_token = 'YOURTOKEN'
-access_token_secret = 'YOURTOKENSECRET'
+access_token = '881921522981642240-qgG0HddwwkI2YImAzA7XdKobmLtjD5l'
+access_token_secret = 'YJ5OEOYicHgB8f99VNGd1bKSYPekqv882xj8Nf3dG5Asx'
 
 consumer = oauth.Consumer(key=consumer_key, secret=consumer_secret)
 access_token = oauth.Token(key=access_token, secret=access_token_secret)
 client = oauth.Client(consumer, access_token)
 
-timeline_endpoint = 'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=tofire&count=10&exclude_replies=true&include_rts=false'
+timeline_endpoint = 'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=tofire&count=20&exclude_replies=true&include_rts=false'
 
 response, data = client.request(timeline_endpoint)
 
